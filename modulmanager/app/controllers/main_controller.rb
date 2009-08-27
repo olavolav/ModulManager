043594307df61f93d5eddab9909078ceb8239661
cbeulke <mail@beulke.net>
2009-08-27 09:58:06 +0000

    *Fehlerkorrektur und Erweiterung* des _MainController_ und _IndexView_.
    git-svn-id: file:///Users/olav/Downloads/svntemp/mmsvn/trunk@14 8b7659b4-8212-416e-ac94-f2f9a277eaa9

--- modulmanager/app/controllers/main_controller.rb
+++ modulmanager/app/controllers/main_controller.rb
@@ -3,11 +3,8 @@ class MainController < ApplicationController
   # Stellt für den View vorerst alle Module in unsortierter Ordnung in @modules
   # zur Verfügung
   def index
-
     @modules = Studmodule.all
-
-    render "index"
-
+    @categories = Category.all
   end
 
 end
