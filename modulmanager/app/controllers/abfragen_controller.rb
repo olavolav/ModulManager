76d18f679fec2ba200302558a88d6ec4eedb8ad3
cbeulke <mail@beulke.net>
2009-08-27 13:34:44 +0000

    Die Action _rechts_ im *AbfragenController* liefert nun alle Module Baumartig unter die jeweiligen Kategorien (Pflichtmodule, Schlüsselqulifikationen, etc.) untergeordnet als XML zurück.
    git-svn-id: file:///Users/olav/Downloads/svntemp/mmsvn/trunk@15 8b7659b4-8212-416e-ac94-f2f9a277eaa9

--- modulmanager/app/controllers/abfragen_controller.rb
+++ modulmanager/app/controllers/abfragen_controller.rb
@@ -6,6 +6,32 @@ class AbfragenController < ApplicationController
   end
 
   def rechts
+    @data = generate_right_list
+    respond_to do |format|
+      # format.html
+      format.xml { render :xml => @data.to_xml }
+    end
+  end
+
+  private
+
+  def generate_right_list
+    root = Category.find(:first, :conditions => "category_id IS null")
+    return rekursiv(root, Hash.new)
+  end
+
+  def rekursiv(parent, hash)
+    if(parent.categories != [])
+      i = 0
+      parent.categories.each do |c|
+        i += 1
+        hash[c.name] = Hash.new
+        hash[c.name] = rekursiv(c, hash[c.name])
+      end
+    else
+      return parent.studmodules
+    end
+    return hash
   end
 
 end
--- modulmanager/app/models/category.rb
+++ modulmanager/app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ActiveRecord::Base
   belongs_to :category, { :foreign_key => "category_id" }
-  has_many :cildren, { :foreign_key => "category_id" }
+  has_many :categories, { :foreign_key => "category_id" }
+  has_many :studmodules, { :foreign_key => "category_id" }
 end
