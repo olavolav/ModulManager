505b3448ccf86baae02f7831e2f78f3229773908
QuanNguyen <nvquan2002@yahoo.de>
2009-09-02 18:33:30 +0000

    JS-Datei Änderung für Pool
    git-svn-id: file:///Users/olav/Downloads/svntemp/mmsvn/trunk@32 8b7659b4-8212-416e-ac94-f2f9a277eaa9

--- modulmanager/public/javascripts/pooleffect.js
+++ modulmanager/public/javascripts/pooleffect.js
@@ -3,12 +3,44 @@
 
 $(function(){
 	
+	/*------------Bachelor--------------------------------------------------------------*/
+			
+			$(".Bachelor .Bachelor-Arbeit").hide();
+			$(".Bachelor .Kerncurriculum").hide();
+			$(".Bachelor .Spezialisierung").hide();
+			
+			
+			
+			$('.Bachelor a').live("click",function(){
+				
+				$(".Bachelor .Bachelor-Arbeit").toggle("slow");
+				$(".Bachelor .Kerncurriculum").toggle("slow");
+				$(".Bachelor .Spezialisierung").toggle("slow");
+				
+				
+				
+		
+			});
+	
 	/*------------Pflichtmodule---------------------------------------------------------*/
+			
+		/*	$(".Pflichtmodule .Grundkurse").hide();
+			$(".Pflichtmodule .Praktika").hide();
+			$(".Pflichtmodule .Mathematik").hide();	
+			
+			$(".Pflichtmodule a").live("click",function(){
+				$(".Pflichtmodule .Grundkurse").toggle("slow");
+				$(".Pflichtmodule .Praktika").toggle("slow");
+				$(".Pflichtmodule .Mathematik").toggle("slow");
+				
+			});
+		*/
+	
 		/*--------Grundkurse-----------------------------------------------*/
 		
-			$(".Grundkurse ul").hide();
-			$('.Grundkurse').live("click",function(){
-				$(".Grundkurse ul").toggle();
+			$(".Grundkurse  ul").hide();
+			$('.Grundkurse ').live("click",function(){
+				$(".Grundkurse  ul").toggle("slow");
 		
 			});
 	
@@ -16,7 +48,7 @@ $(function(){
 	
 			$(".Praktika ul").hide();
 			$('.Praktika').live("click",function(){
-				$(".Praktika ul").toggle();
+				$(".Praktika ul").toggle("slow");
 		
 			});
 			
@@ -24,7 +56,7 @@ $(function(){
 	
 			$(".Mathematik ul").hide();
 			$('.Mathematik').live("click",function(){
-				$(".Mathematik ul").toggle();
+			$(".Mathematik ul").toggle("slow");
 		
 			});
 			
@@ -33,21 +65,21 @@ $(function(){
 			
 			$(".Spezialisierungspraktikum ul").hide();
 			$(".Spezialisierungspraktikum").live("click",function(){
-				$(".Spezialisierungspraktikum ul").toggle();
+				$(".Spezialisierungspraktikum ul").toggle("slow");
 			});
 			
 		/*--------Einfuehrungen-----------------------------------------------*/
 			
 			$(".einfuehrungen ul").hide();
 			$(".einfuehrungen").live("click",function(){
-				$(".einfuehrungen ul").toggle();
+				$(".einfuehrungen ul").toggle("slow");
 			});
 			
 		/*--------Spezielle Themen-----------------------------------------------*/
 			
 			$(".spezielle ul").hide();
 			$(".spezielle").live("click",function(){
-				$(".spezielle ul").toggle();
+				$(".spezielle ul").toggle("slow");
 			});
 			
 	////////////////////////////////////////////////////////////////////////////////////////////////
@@ -56,14 +88,14 @@ $(function(){
 	
 			$(".Profilierungsbereich ul").hide();
 			$(".Profilierungsbereich").live("click",function(){
-				$(".Profilierungsbereich ul").toggle();
+				$(".Profilierungsbereich ul").toggle("slow");
 			});
 			
 	/*------------Schlüsselkompetenzen---------------------------------------------------------*/
 	
 			$(".schluesselkompetenzen ul").hide();
 			$(".schluesselkompetenzen").live("click",function(){
-				$(".schluesselkompetenzen ul").toggle();
+				$(".schluesselkompetenzen ul").toggle("slow");
 			});
 
 
@@ -71,36 +103,49 @@ $(function(){
 
 			$(".astro ul").hide();
 			$(".astro").live("click",function(){
-				$(".astro ul").toggle();
+				$(".astro ul").toggle("slow");
 			});
 			
 	/*------------Biophysik und Physik komplexer Systeme---------------------------------------------------------*/
 
 			$(".biophysik ul").hide();
 			$(".biophysik").live("click",function(){
-				$(".biophysik ul").toggle();
+				$(".biophysik ul").toggle("slow");
 			});		
 			
 	/*------------Festkörper- und Materialphysik---------------------------------------------------------*/
 
 			$(".materialphysik ul").hide();
 			$(".materialphysik").live("click",function(){
-				$(".materialphysik ul").toggle();
+				$(".materialphysik ul").toggle("slow");
 			});	
 			
 	/*------------Kern- und Teilchenphysik---------------------------------------------------------*/
 
 			$(".teilchenphysik ul").hide();
 			$(".teilchenphysik").live("click",function(){
-				$(".teilchenphysik ul").toggle();
+				$(".teilchenphysik ul").toggle("slow");
 			});			
 					
 			
 });//ende global function
 
+	/*----------POOL Drag and Drop---------------------------------------------------------------------------*/
+	
+	$(function(){
+		
+		$("ul li").draggable({
+			revert : "invalid"
+			
+			
+		});
+		
+		
+	});
 
 
-	/*----------AJAX Teil---------------------------------------------------------------------------*/
+
+	/*----------AJAX aufrufen wenn ein Modul in Auswahl reinziehen---------------------------------------------------------------------------*/
 	
 	$(function(){
 
@@ -110,13 +155,19 @@ $(function(){
                                         hoverClass: 'drophover',
 
                                         drop: function(event, ui){
-
-                                                $(ui.draggable).hide();
-
-
-                                                $.ajax({
+												
+												// id von reingezogenem Modul und entsprechendem Semester holen
+												
+												var modulID = $(ui.draggable).attr("id"); 
+												var semester = $(this).attr("id");
+												$(ui.draggable).hide();
+                                                
+												
+												// DATEN mit Modul-ID und semester zum Server schicken 
+												// xml enthaltet die XML-DATEI  von abfragen/auswahl. Hier mit pool zum Testen
+                                                var xml=$.ajax({
                                                         type: 'GET',
-
+														//URL hier eigentlich abfragen/auswahl, hier mit pool zum Testen
                                                         url : 'http://localhost:3000/abfragen/pool',
 
                                                         cache:false,
@@ -124,19 +175,60 @@ $(function(){
                                                         async :false,
 
                                                         contentType:'application/x-www-form-urlencoded',
+														error: function(a, b, c){
+														
+															alert("Erorr");
+															
+														}
+                                                        
 
-                                                        success: function(msg){
-                                                               rekursiv(msg);
-                                                        },
-                                                        error: function (a,b,c){
-                                                                alert("Erorr");
-                                                        }
-
-                                                });//ende Ajax
+                                                }).responseXML;//ende Ajax
+                                                
+												
+												
+												//-----Implementierung der Funktion auswahlAnzeige() mit XML-Datei von abfragen/auswahl******************
+												// hier mit abfragen/pool zum Testen
+												// immer noch bei DROP in DROPPABLE 
+												
+												var root = xml.documentElement;
+												
+												var auswahlAnzeige = function (modulID,semester,root){
+													// var semester ist ID für das entsprechem Semester-Box
+													$(root).find("module").each( function(){
+														// suche module mit modulID
+														
+														if ( $(this).find("id").text() == modulID ){
+															var name  = $(this).find("name").text();
+															var short = $(this).find("short").text();
+															var credits = $(this).find("credits").text();
+															
+															// das Modul in entsprechenem Semester hineinfügen
+															
+															$("#"+semester).append("<div class='auswahlmodul'>"+name+" "+short+" "+credits+"</div>");
+														}
+														
+														
+													});//ende each bei Suche nach ID von einem Modul
+												}//ende auswahlAnzeige
+                                                
+												auswahlAnzeige(modulID,semester,root);	
+													
+													
+													
+													
+													
+												
+												
+												
+                                                
+												
+                                                
+												
+												
                                          }//ende Drop
                                   });//ende droppable
 
-
+								
 
 
                                         $('.modul').draggable({
@@ -174,19 +266,7 @@ $(function(){
 	
 	
 	
-	/*----------POOL Drag and Drop---------------------------------------------------------------------------*/
 	
-	$(function(){
-		
-		$("ul li").draggable({
-			revert : "invalid"
-			
-		});
-		
-		
-	});
-
-
 
 
 
