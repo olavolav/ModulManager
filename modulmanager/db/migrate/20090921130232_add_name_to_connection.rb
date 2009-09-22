6e2504c7cb10335c9d30d697f66824a4658a9516
cbeulke <mail@beulke.net>
2009-09-22 11:32:41 +0000

    Eine Datei gelöscht
    git-svn-id: file:///Users/olav/Downloads/svntemp/mmsvn/trunk@109 8b7659b4-8212-416e-ac94-f2f9a277eaa9

--- modulmanager/db/migrate/20090921130232_add_name_to_connection.rb
+++
@@ -1,9 +0,0 @@
-class AddNameToConnection < ActiveRecord::Migration
-  def self.up
-    add_column :connections, :name, :string
-  end
-
-  def self.down
-    remove_column :connections, :name
-  end
-end
