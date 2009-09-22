38ffac5378e46f578f9978a9d20080e9e64933fa
cbeulke <mail@beulke.net>
2009-09-22 13:17:51 +0000

    Layout für Hauptseite eingerichtet
    git-svn-id: file:///Users/olav/Downloads/svntemp/mmsvn/trunk@110 8b7659b4-8212-416e-ac94-f2f9a277eaa9

--- modulmanager/app/controllers/main_controller.rb
+++ modulmanager/app/controllers/main_controller.rb
@@ -1,18 +1,40 @@
 class MainController < ApplicationController
 
   def index
+    respond_to do |format|
+      format.html { render :layout => "modulmanager" }
+    end
   end
 
   def start
+
+    # cs = current_selection
+
   end
 
   def help
   end
 
   def export
+    
+
+  end
+
+  def get_file
+    @selection = current_selection
+    headers["Content-Type"] = "application/stpl"
+    headers["Content-Disposition"] = "attachment; filename=export.stpl"
+    render :xml => "Hallo Welt!"
   end
 
   def import
+  end
+
+  private
+  
+  def current_selection
+    session[:selection_id] ||= new_selection
+    ModuleSelection.find session[:selection_id]
   end
 
 end
