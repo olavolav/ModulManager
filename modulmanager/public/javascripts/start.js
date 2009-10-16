7b4d2394a3bfeb40828c09c4340ffd97bbe3c6b1
olavolav <olav.stetter@googlemail.com>
2009-10-16 10:13:26 +0000

    Buttons auf der Haupt-Seite umgestellt, Buttons auf der Startseite zur Hälfte (also SP, PO noch nciht) umgestellt, funktioniert aber noch nicht mit der AJAX-Anfrage (2. Versuch)
    git-svn-id: file:///Users/olav/Downloads/svntemp/mmsvn/trunk@275 8b7659b4-8212-416e-ac94-f2f9a277eaa9

---
+++ modulmanager/public/javascripts/start.js
@@ -0,0 +1,21 @@
+var update_schwerpunkt = function(s_id){
+	
+	$(".schwerpunkt_oben").css("background","white");
+
+    alert("hallo schwerpunkt "+s_id);
+
+    $.ajax({
+        type:"POST",
+        url :"abfragen/focus_selection",
+        dataType:"text",
+        cache:false,
+        async:false,
+        data:s_id,
+        contentType:'application/x-www-form-urlencoded',
+        error : function(a,b,c){
+            alert ("error mit update_schwerpunkt");
+        }
+    });
+	
+	$(".schwerpunkt_oben[id="+s_id+"]").css("background","#d8eef7");
+}
