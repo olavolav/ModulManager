f39ca598622d0e29d73ac19de496ae861368299f
cbeulke <mail@beulke.net>
2010-03-08 10:51:51 +0000

    Gruppen-Checkboxen erstmal komplett entfernt, da es dort noch Fehler gibt
    git-svn-id: file:///Users/olav/Downloads/svntemp/mmsvn/trunk@550 8b7659b4-8212-416e-ac94-f2f9a277eaa9

--- modulmanager/app/controllers/main_controller.rb
+++ modulmanager/app/controllers/main_controller.rb
@@ -156,15 +156,16 @@ class MainController < ApplicationController
   def _check_category
     id = params[:mod_id]
     mod = Studmodule.find(id)
-#    @categories = Array.new
+    @categories = Array.new
 #    Category.find(:all, :conditions => "exclusive = 1").each do |category|
 #      @categories.push category
 #    end
-    mod.categories.each do |category|
-      if category.exclusive
-        @categories.push category
-      end
-    end
+
+#    mod.categories.each do |category|
+#      if category.exclusive
+#        @categories.push category
+#      end
+#    end
 
     respond_to do |format|
       format.html { render :action => "_check_category", :layout => false }
