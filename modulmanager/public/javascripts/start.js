ad0bc9e9c4f84f7cdf299b0a893226e3c733045e
Olav Stetter <olav.stetter@googlemail.com>
2012-03-08 14:30:21 +0100

    Bugfix: Auswahl des richtigen SP nach PO-Wechsel Das Zurücksetzen des SP nach Wechsel der PO hatte nicht richtig funktioniert, durch falsches JavaScript und nicht geschlossenes PO-<div>

--- modulmanager/public/javascripts/start.js
+++ modulmanager/public/javascripts/start.js
@@ -2,19 +2,16 @@
 for licensing questions please refer to the README
 Created by Christian Beulke, Van Quan Nguyen and Olav Stetter */
 
-
-// Falls JavaScript funktioniert, die entspr. Warnung verstecken (OS)
 $(document).ready(function(){
+  // Falls JavaScript funktioniert, die entspr. Warnung verstecken (OS)
 	$("#javascriptdoesnotwork").css("display","none");
 	
-	$("#pruefungsordnung input").change( function() {
-    // alert($("#pruefungsordnung input:checked").attr("id"));
-	  update_pordnung( $("#pruefungsordnung input:checked").attr("id") );
+  // An die PO- und SP-Auswahl-Radiobuttons die jew. Aktualisierungs-Funktionen knüpfen
+	$("#pruefungsordnung input:radio").change( function() {
+	  update_pordnung( $(this).attr("id") );
 	});
-
-	$("#schwerpunkt input").change( function() {
-    // alert($("#pruefungsordnung input:checked").attr("id"));
-	  update_schwerpunkt( $("#schwerpunkt input:checked").attr("id") );
+	$("#schwerpunkt input:radio").change( function() {
+    update_schwerpunkt( $(this).attr("id") );
 	});
 
 });
@@ -26,51 +23,47 @@ var authenticityToken = function() {
 var authenticityTokenParameter = function(){
    return 'authenticity_token=' + encodeURIComponent(authenticityToken());
 };
-var update_schwerpunkt = function(s_id){
-	
-	if ($("#schwerpunkt input[id="+s_id+"]").val() != "checked") {
-    // alert("hallo schwerpunkt "+s_id);
-    $("#serverwait").show();
-		$("#schwerpunkt a").removeClass("btn-info");
-    $.ajax({
-        type:"POST",
-        url :"main/focus_selection",
-        dataType:"text",
-        cache:false,
-        async:false,
-        data:"id="+s_id+"&"+authenticityTokenParameter(),
-        contentType:'application/x-www-form-urlencoded',
-				error: function(XMLHttpRequest, textStatus, errorThrown) {
-					ajax_serverupdate_on_AJAX_warning("textStatus="+textStatus+",fn=update_schwerpunkt");
-				}
-    });
-    $("#serverwait").fadeOut();
-	}
+
+function update_schwerpunkt(s_id) {
+  // alert("hallo schwerpunkt "+s_id);
+  $("#serverwait").show();
+  $.ajax({
+    type:"POST",
+    url :"main/focus_selection",
+    dataType:"text",
+    cache:false,
+    async:false,
+    data:"id="+s_id+"&"+authenticityTokenParameter(),
+    contentType:'application/x-www-form-urlencoded',
+    error: function(XMLHttpRequest, textStatus, errorThrown) {
+    	ajax_serverupdate_on_AJAX_warning("textStatus="+textStatus+",fn=update_schwerpunkt");
+    }
+  });
+  $("#serverwait").fadeOut();
 };
 
-var update_pordnung = function(po_id){
-	
-	if ($("#pruefungsordnung input[id="+po_id+"]").val() != "checked") {
-    // alert("hallo pruefungsordnung "+po_id);
-		$("#serverwait").show();
-    $.ajax({
-        type:"POST",
-        url :"main/version_selection",
-        dataType:"text",
-        cache:false,
-        async:false,
-        data:"version="+po_id+"&"+authenticityTokenParameter(),
-        contentType:'application/x-www-form-urlencoded',
-				error: function(XMLHttpRequest, textStatus, errorThrown) {
-					ajax_serverupdate_on_AJAX_warning("textStatus="+textStatus+",fn=update_pordnung");
-				}
-    });
-		$("#schwerpunkt .SPsubclass").hide();
-    // Schwerpunkt-Radio-Button zurück setzen
-		$("#schwerpunkt .SPsubclass[id=belongstoPOid"+po_id+"] input[id=-1]").attr("checked","checked");
-		$("#schwerpunkt .SPsubclass[id=belongstoPOid"+po_id+"]").show();
-		$("#serverwait").fadeOut();
-	}
+function update_pordnung(po_id) {
+  // alert("hallo pruefungsordnung "+po_id);
+  $("#serverwait").show();
+  $.ajax({
+    type:"POST",
+    url :"main/version_selection",
+    dataType:"text",
+    cache:false,
+    async:false,
+    data:"version="+po_id+"&"+authenticityTokenParameter(),
+    contentType:'application/x-www-form-urlencoded',
+		error: function(XMLHttpRequest, textStatus, errorThrown) {
+			ajax_serverupdate_on_AJAX_warning("textStatus="+textStatus+",fn=update_pordnung");
+		}
+  });
+  $("#schwerpunkt .SPsubclass").hide();
+
+  // Schwerpunkt-Radio-Button zurück setzen
+  $("#schwerpunkt .SPsubclass[id=belongstoPOid"+po_id+"] input[id=-1]").attr("checked","checked");
+
+  $("#schwerpunkt .SPsubclass[id=belongstoPOid"+po_id+"]").show();
+  $("#serverwait").fadeOut();
 };
 
 var ajax_serverupdate_on_AJAX_warning = function(text){
