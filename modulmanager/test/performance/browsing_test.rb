9045a0d0968d385c94d21acc78f7b4d96a81adbc
Olav Stetter <olav.stetter@googlemail.com>
2013-06-03 21:24:39 +0200

    Added minimal performance tests.

--- modulmanager/test/performance/browsing_test.rb
+++ modulmanager/test/performance/browsing_test.rb
@@ -3,7 +3,12 @@ require 'performance_test_help'
 
 # Profiling results for each test method are written to tmp/performance.
 class BrowsingTest < ActionController::PerformanceTest
-  def test_homepage
+  def test_startup
     get '/'
   end
+
+  def test_main_page
+    get '/main'
+  end
+
 end
